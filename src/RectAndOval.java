2b5004df1ba38f7a8dae2034e516a6b8d5749ed7
SugayaShion <zeal24360050@fsgcl.jp>
2025-05-26 11:33:28 +0900

    練習問題3-1

---
+++ src/RectAndOval.java
@@ -0,0 +1,10 @@
+
+public class RectAndOval extends MyFrame{
+	public void run() {
+		fillRect(55,100,20,20);
+		fillRect(45,110,40,10);
+		fillOval(50,120,10,10);
+		fillOval(70,120,10,10);
+	}
+
+}
--- src/RectTest.java
+++ src/RectTest.java
@@ -3,14 +3,19 @@ public class RectTest extends MyFrame{
 	public void run() {
 		setColor(255,255,0);
 		fillRect(30,40,50,100);
+		
 		setColor(150,0,200);
 		fillRect(130,40,50,100);
+		
 		setColor(0,255,255);
 		fillRect(30,200,150,10);
+		
 		setColor(0,0,0);
 		fillRect(90,40,30,30);
+		
 		setColor(100,100,100);
 		fillRect(90,80,30,30);
+		
 		setColor(255,255,255);
 		fillRect(40,50,25,25);
 	}
