ca75c848726ecba5ef3d20e8cc0c2646cfb1815b
SugayaShion <zeal24360050@fsgcl.jp>
2025-05-26 12:13:32 +0900

    RectIf.javaとRectLoop.javaの作成

---
+++ src/RectIf.java
@@ -0,0 +1,13 @@
+
+public class RectIf extends MyFrame{
+	public void run() {
+		int x=30;
+		setColor(0,128,0);
+		int i;
+		for (i=0 ; i<9; i++) {
+			fillRect(x,80,10,100);
+			x+=20;
+		}
+	}
+
+}
