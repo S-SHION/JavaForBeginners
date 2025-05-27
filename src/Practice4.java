5b085bd51fb7d4bc1085a289b6500bf937ec2db7
SugayaShion <zeal24360050@fsgcl.jp>
2025-05-27 10:34:16 +0900

    課題EX修正

--- src/Practice4.java
+++ src/Practice4.java
@@ -5,9 +5,9 @@ public class Practice4 extends MyFrame{
 		int i;
 		for (i=0 ; i<20; i++) {
 			if(i<10) {
-			    setColor(x,x,x);
+			    setColor(x-10,x-10,x-10);
 			}else {
-				setColor(x-40*(i-10),x-40*(i-10),x-40*(i-10));
+				setColor(x-10-40*(i-10),x-10-40*(i-10),x-10-40*(i-10));
 			}
 			fillRect(x,80,10,100);
 			x+=20;
