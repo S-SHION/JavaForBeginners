7815a3158b4f1fdb5e0e7a54979f326d1398a12f
SugayaShion <zeal24360050@fsgcl.jp>
2025-06-10 11:31:07 +0900

    練習問題9

--- src/Car.java
+++ src/Car.java
@@ -1,5 +1,7 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Car extends Vehicle{
+public class Car extends Vehicle implements KeyListener{
 	public Car (int x,int y,int vx,int vy) {
 		super(x, y, vx, vy);
 	}
@@ -8,6 +10,48 @@ public class Car extends Vehicle{
 		frame.fillRect(x-10,y+10,40,10);
 		frame.fillOval(x-5, y+20, 10, 10);
 		frame.fillOval(x+15, y+20, 10, 10);
+	}
+	
+	//キーボードから文字が入力されたときに実行
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("文字が入力されました。");
+	}
+	//キーが押されたときに実行
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx=-5;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx=5;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_UP) {
+			vy=-5;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			vy=5;
+		}
+	}
+	//押されたキーが離されたときに実行
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			vx=0;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			vx=0;
+		}
+		if(e.getKeyCode() == KeyEvent.VK_UP) {
+			vy=0;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			vy=0;
+		}
+		
 	}
 	
 	/*public void move() {
